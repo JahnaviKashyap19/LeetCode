83a0a95e315926480da3aea8d9f1b18f6646a5a1
Jahnavi Kashyap <57027274+JahnaviKashyap19@users.noreply.github.com>
2022-08-09 16:56:55 +0530

    Time: 133 ms (81.05%), Space: 67.8 MB (54.07%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.cpp
+++ 53-maximum-subarray/53-maximum-subarray.cpp
@@ -1,13 +1,46 @@
 class Solution {
 public:
-    int maxSubArray(vector<int>& nums) {
-        int cur_sum=0;
-        int sum=INT_MIN;
-        for(int i=0;i<nums.size();i++)
-        {
-            cur_sum=max(nums[i],cur_sum+nums[i]);
-            sum=max(sum,cur_sum);
-        }
-        return sum;
+//Brute Force
+//     int maxSubArray(vector<int>& nums) {
+//         int n=nums.size();
+//         if(n==0)return 0;
+//         if(n==1)return nums[0];
+//         int res=nums[0];
+//         for(int i=0;i<n;i++){
+//             for(int j=i;j<n;j++){
+//                 int sum=0;
+//                 for(int k=i;k<=j;k++){
+//                     sum+=nums[k];
+//                 }
+//                 res=max(sum,res);
+//             }
+//         }
+//         return res;      
+//     }
+//     int maxSubArray(vector<int>& nums){
+// optimal soln
+//         int n=nums.size(),res=INT_MIN;
+//         if(n==1)return nums[0];
+//         for(int i=0;i<n;i++){
+//             int sum=0;
+//             for(int j=i;j<n;j++){
+//                 sum+=nums[j];
+//                 res=max(res,sum);
+//             }
+            
+//         }
+//        return res; 
+//     }
+   int maxSubArray(vector<int>& nums){
+    int n=nums.size();
+    int sum=0,maxi=INT_MIN;
+    for(auto i:nums){
+        sum+=i;
+        maxi=max(sum,maxi);
+        if(sum<0)sum=0;
+        
     }
+       return maxi;
+   } 
+    
 };
